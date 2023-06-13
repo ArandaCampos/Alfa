ca3994b85f994be122610e090f3b56dc96f2a759
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2023-06-13 17:10:19 -0300

    remove Hover

--- alfa/animations.py
+++ alfa/animations.py
@@ -8,14 +8,3 @@ class Blink():
     def play(self, surface = None, rendered = None):
         self.alpha = self.alpha - self.velocity if self.alpha >= self.velocity else 255
         surface.set_alpha(self.alpha)
-
-class Hover():
-    def __init__(self, first_color, secound_color):
-        self.first_color = first_color
-        self.color_on_hover = secound_color
-
-    def get_events(self, surface = None, rendered = None):
-        try:
-            return rendered.collidepoint(pygame.mouse.get_pos())
-        except AttributeError:
-            return False
